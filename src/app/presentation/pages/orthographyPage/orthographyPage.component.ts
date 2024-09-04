275c0888fc0057e470cfa8b877f121957ad0423e
alemoles <alemolerio@gmail.com>
2024-09-03 22:42:17 -0300

    code refactor for file routes

--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ChatMessageComponent } from '../../components/chat-bubbles/chatMessage/chatMessage.component';
-import { MyMessageComponent } from '../../components/chat-bubbles/myMessage/myMessage.component';
+import { ChatMessageComponent, MyMessageComponent } from '@components/index';
+
 
 @Component({
   selector: 'app-orthography-page',
