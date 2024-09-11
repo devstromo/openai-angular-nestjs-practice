f82b9d79280cd10f7f9f4bca292b605fcd2ed610
alemoles <alemolerio@gmail.com>
2024-09-10 23:02:15 -0300

    create chat template

---
+++ src/app/template/chatTemplate/chatTemplate.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TextMessageEvent, TextMessageBoxEvent, ChatMessageComponent, MyMessageComponent, TypingLoaderComponent, TextMessageBoxComponent } from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
+
+@Component({
+  selector: 'app-chat-template',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
+  ],
+  templateUrl: './chatTemplate.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ChatTemplateComponent {
+  public messages = signal<Message[]>([{ text: 'Hola Mundo', isGpt: true }]);
+  public isLoading = signal(false);
+  public OpenAiService = inject(OpenAiService);
+
+  handleMessage(prompt: string) {
+    console.log({ prompt });
+
+  }
+  // handleMessageWithFile({ prompt, file }: TextMessageEvent) {
+  //   console.log({ prompt, file });
+
+  // }
+
+  // handleMessageWithSelect(event: TextMessageBoxEvent) {
+  //   console.log({ event });
+
+  // }
+}
