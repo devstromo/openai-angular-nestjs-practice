fec62fb01b3918bffee8dfa647ac485bd658a9c1
alemoles <alemolerio@gmail.com>
2024-09-29 19:00:46 -0300

    create pros cons response interface

--- src/app/presentation/pages/prosConsPage/prosConsPage.component.ts
+++ src/app/presentation/pages/prosConsPage/prosConsPage.component.ts
@@ -1,13 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChatMessageComponent, MyMessageComponent, TypingLoaderComponent, TextMessageBoxComponent } from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-pros-cons-page',
   standalone: true,
   imports: [
     CommonModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
   ],
   templateUrl: './prosConsPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ProsConsPageComponent { }
+export default class ProsConsPageComponent {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public OpenAiService = inject(OpenAiService);
+
+  handleMessage(prompt: string) {
+    console.log({ prompt });
+
+  }
+ }
