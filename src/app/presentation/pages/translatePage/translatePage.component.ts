7a4a7512c6ed63042b58612f9df3ec279ed2de36
alemoles <alemolerio@gmail.com>
2024-10-07 20:17:22 -0300

    set up translate page component

--- src/app/presentation/pages/translatePage/translatePage.component.ts
+++ src/app/presentation/pages/translatePage/translatePage.component.ts
@@ -1,13 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ChatMessageComponent, MyMessageComponent, TypingLoaderComponent, TextMessageBoxComponent, TextMessageBoxEvent, TextMessageBoxSelectComponent } from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-translate-page',
   standalone: true,
   imports: [
     CommonModule,
-  ],
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
+    TextMessageBoxSelectComponent
+],
   templateUrl: './translatePage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class TranslatePageComponent { }
+export default class TranslatePageComponent {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public OpenAiService = inject(OpenAiService);
+
+  handleMessageWithSelect(event: TextMessageBoxEvent) {
+    console.log({ event });
+
+  }
+}
