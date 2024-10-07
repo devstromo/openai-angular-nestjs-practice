6cc1cd1ab0840f749aa7767ba7b8b65c1f972e8e
alemoles <alemolerio@gmail.com>
2024-10-07 20:49:50 -0300

    implement UI for translate page component

--- src/app/presentation/pages/translatePage/translatePage.component.ts
+++ src/app/presentation/pages/translatePage/translatePage.component.ts
@@ -16,17 +16,34 @@ import { OpenAiService } from 'app/presentation/services/openai.service';
     TypingLoaderComponent,
     TextMessageBoxComponent,
     TextMessageBoxSelectComponent
-],
+  ],
   templateUrl: './translatePage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class TranslatePageComponent {
   public messages = signal<Message[]>([]);
   public isLoading = signal(false);
-  public OpenAiService = inject(OpenAiService);
-
-  handleMessageWithSelect(event: TextMessageBoxEvent) {
-    console.log({ event });
+  public openAiService = inject(OpenAiService);
+  public languages = signal([
+    { id: 'alemán', text: 'Alemán' },
+    { id: 'árabe', text: 'Árabe' },
+    { id: 'bengalí', text: 'Bengalí' },
+    { id: 'francés', text: 'Francés' },
+    { id: 'hindi', text: 'Hindi' },
+    { id: 'inglés', text: 'Inglés' },
+    { id: 'japonés', text: 'Japonés' },
+    { id: 'mandarín', text: 'Mandarín' },
+    { id: 'portugués', text: 'Portugués' },
+    { id: 'ruso', text: 'Ruso' },
+  ]);
+  handleMessageWithSelect({ prompt, selectedOption }: TextMessageBoxEvent) {
+    const message = `Traduce el texto: ${prompt} en ${selectedOption}`;
+    this.isLoading.set(true);
+    this.messages.update((prev) => [...prev, { text: message, isGpt: false }]);
+    this.openAiService.translateText(prompt, selectedOption).subscribe(({ message }) => {
+      this.isLoading.set(false);
+      this.messages.update((prev) => [...prev, { text: message, isGpt: true }]);
+    })
 
   }
 }
