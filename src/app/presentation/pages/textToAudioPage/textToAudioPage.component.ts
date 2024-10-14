1af24447dd924b61afabf2f48c64c84391d9d608
alemoles <alemolerio@gmail.com>
2024-10-14 20:34:35 -0300

    set up text to audio page

--- src/app/presentation/pages/textToAudioPage/textToAudioPage.component.ts
+++ src/app/presentation/pages/textToAudioPage/textToAudioPage.component.ts
@@ -1,13 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ChatMessageComponent, MyMessageComponent, TypingLoaderComponent, TextMessageBoxComponent, TextMessageBoxSelectComponent, TextMessageBoxEvent } from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-text-to-audio-page',
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxSelectComponent
   ],
   templateUrl: './textToAudioPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class TextToAudioPageComponent { }
+export default class TextToAudioPageComponent {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public OpenAiService = inject(OpenAiService);
+
+  handleMessageWithSelect(event: TextMessageBoxEvent) {
+    console.log({ event });
+
+  }
+}
