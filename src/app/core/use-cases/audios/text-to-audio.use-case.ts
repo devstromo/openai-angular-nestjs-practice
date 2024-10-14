d0b4069ae14c65c130db8dca55b5c144e7826b42
alemoles <alemolerio@gmail.com>
2024-10-14 20:40:10 -0300

    create text to use  use case

---
+++ src/app/core/use-cases/audios/text-to-audio.use-case.ts
@@ -0,0 +1,36 @@
+import { environment } from "environments/environment.development";
+
+export const textToAudioUserCase = async (prompt: string, voice: string) => {
+
+    try {
+        const resp = await fetch(
+            `${environment.backendApi}/text-to-audio`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ prompt, voice }),
+            }
+        );
+        if (!resp.ok) {
+            throw new Error('No se pudo generar el audio');
+        }
+
+        const audioFile = await resp.blob();
+        const audioUrl = URL.createObjectURL(audioFile);
+        return {
+            ok: true,
+            message: prompt,
+            audioUrl: audioUrl
+        };
+
+    } catch (error) {
+        console.log(error);
+        return {
+            ok: false,
+            message: 'No se pudo generar el audio',
+            audioUrl: ''
+        };
+    }
+};
--- src/app/core/use-cases/index.ts
+++ src/app/core/use-cases/index.ts
@@ -2,3 +2,4 @@ export * from './orthography/orthography.use-case';
 export * from './pros-cons/pros-cons-discusser.use-case';
 export * from './pros-cons/pros-cons-stream.use-case';
 export * from './translate/translate-text.use-case';
+export * from './audios/text-to-audio.use-case';
