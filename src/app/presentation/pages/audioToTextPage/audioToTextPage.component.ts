2e57534754085b835fd0de94171ce66447bc3df6
alemoles <alemolerio@gmail.com>
2024-10-21 19:44:10 -0300

    set up audio to text page

--- src/app/presentation/pages/audioToTextPage/audioToTextPage.component.ts
+++ src/app/presentation/pages/audioToTextPage/audioToTextPage.component.ts
@@ -1,13 +1,36 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ChatMessageComponent, MyMessageComponent, TypingLoaderComponent, TextMessageEvent } from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
+import { TextMessageBoxFileComponent } from "../../components/text-boxes/textMessageBoxFile/textMessageBoxFile.component";
 
 @Component({
   selector: 'app-audio-to-text-page',
   standalone: true,
   imports: [
     CommonModule,
-  ],
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxFileComponent
+],
   templateUrl: './audioToTextPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AudioToTextPageComponent { }
+export default class AudioToTextPageComponent {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public OpenAiService = inject(OpenAiService);
+
+  handleMessage(prompt: string) {
+    console.log({ prompt });
+
+  }
+  handleMessageWithFile({ prompt, file }: TextMessageEvent) {
+    console.log({ prompt, file });
+  }
+
+}
