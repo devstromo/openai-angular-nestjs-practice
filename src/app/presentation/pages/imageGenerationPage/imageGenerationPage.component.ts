8c2acd0ab3e5303023e1d45ed273ea8fa36293fa
alemoles <alemolerio@gmail.com>
2024-11-04 23:56:35 -0300

    set up image generation component

--- src/app/presentation/pages/imageGenerationPage/imageGenerationPage.component.ts
+++ src/app/presentation/pages/imageGenerationPage/imageGenerationPage.component.ts
@@ -1,13 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ChatMessageComponent, MyMessageComponent, TypingLoaderComponent, TextMessageBoxComponent } from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-image-generation-page',
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
   ],
   templateUrl: './imageGenerationPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ImageGenerationPageComponent { }
+export default class ImageGenerationPageComponent {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public OpenAiService = inject(OpenAiService);
+
+  handleMessage(prompt: string) {
+    console.log({ prompt });
+
+  }
+}
