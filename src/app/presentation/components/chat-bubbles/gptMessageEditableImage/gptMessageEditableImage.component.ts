117d136e71b94ac639c77afdfe094561d24e7252
alemoles <alemolerio@gmail.com>
2024-11-11 20:42:20 -0300

    update image variation component for editable image

--- src/app/presentation/components/chat-bubbles/gptMessageEditableImage/gptMessageEditableImage.component.ts
+++ src/app/presentation/components/chat-bubbles/gptMessageEditableImage/gptMessageEditableImage.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input, Output, ViewChild, signal } from '@angular/core';
 
 @Component({
   selector: 'app-gpt-message-editable-image',
@@ -10,11 +10,36 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from 
   templateUrl: './gptMessageEditableImage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GptMessageEditableImageComponent {
+export class GptMessageEditableImageComponent implements AfterViewInit {
+
   @Input({ required: true }) text!: string;
   @Input({ required: true }) imageInfo!: { url: string, alt: string };
+  @ViewChild('canvas') canvasElement?: ElementRef<HTMLCanvasElement>;
 
   @Output() onSelectedImage = new EventEmitter<string>();
+
+
+  public originalImage = signal<HTMLImageElement | null>(null);
+
+  ngAfterViewInit(): void {
+    if (!this.canvasElement?.nativeElement) return;
+
+    const canvas = this.canvasElement!.nativeElement;
+
+    const ctx = canvas.getContext('2d');
+
+    const image = new Image();
+    image.crossOrigin = 'Anonymous';
+    image.src = this.imageInfo.url;
+
+    this.originalImage.set(image);
+
+    image.onload = () => {
+      ctx?.drawImage(image, 0, 0, canvas.width, canvas.height);
+    }
+
+
+  }
 
   handleClick() {
     this.onSelectedImage.emit(this.imageInfo.url);
