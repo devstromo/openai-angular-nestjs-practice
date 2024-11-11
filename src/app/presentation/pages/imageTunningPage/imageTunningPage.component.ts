3c43b912ba45ca1c4a877ceaa6349858e581edc2
alemoles <alemolerio@gmail.com>
2024-11-11 20:01:10 -0300

    set up image variation UI

--- src/app/presentation/pages/imageTunningPage/imageTunningPage.component.ts
+++ src/app/presentation/pages/imageTunningPage/imageTunningPage.component.ts
@@ -1,13 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ChatMessageComponent, MyMessageComponent, TypingLoaderComponent, TextMessageBoxComponent } from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-image-tunning-page',
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
   ],
   templateUrl: './imageTunningPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ImageTunningPageComponent { }
+export default class ImageTunningPageComponent {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public openAiService = inject(OpenAiService);
+
+  handleMessage(prompt: string) {
+    this.isLoading.set(true);
+    this.messages.update(prev => [...prev, { isGpt: false, text: prompt }]);
+    this.openAiService.imageGeneration(prompt)
+      .subscribe(resp => {
+        this.isLoading.set(false);
+        if (!resp) return;
+
+        this.messages.update(prev => [...prev, { isGpt: true, text: resp.alt, imageInfo: resp }])
+      })
+
+  }
+}
