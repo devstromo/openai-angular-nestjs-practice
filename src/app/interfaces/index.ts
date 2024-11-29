637576eff3f1093df0688a59000dafb01e601f3f
alemoles <alemolerio@gmail.com>
2024-11-28 23:16:21 -0300

    create quesiton response interface for post question use case response

--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -3,3 +3,4 @@ export * from './orthography.response';
 export * from './pros-cons.response';
 export * from './translate-text.response';
 export * from './audio-to-text.response';
+export * from './question.response';
