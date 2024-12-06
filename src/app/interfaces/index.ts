1a6be813a15ed05f40c8d3a22c5d9b85e5873f9e
alemoles <alemolerio@gmail.com>
2024-12-05 22:19:34 -0300

    create BE service response for image to text

--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -4,3 +4,4 @@ export * from './pros-cons.response';
 export * from './translate-text.response';
 export * from './audio-to-text.response';
 export * from './question.response';
+export * from './image-to-text.response';
